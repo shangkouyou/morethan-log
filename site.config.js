0b61236a67130263a7129c1556b1ffd998723958
Scu <73685156+shangkouyou@users.noreply.github.com>
2023-06-21 14:41:55 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/20841687329475_.pic.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
     email: "21@9277.eu.org",
